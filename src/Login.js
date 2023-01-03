150d949c6ba84cdb89bbd3219e7f75c2223d7f60
Mahfuz The Programmer Vetern <mahfuzzayn8@gmail.com>
2023-01-03 21:21:52 +0600

    Second Minor Changes

--- src/Login.js
+++ src/Login.js
@@ -79,7 +79,7 @@ function Login() {
                 </p>
 
                 <button onClick={register} className="login__registerButton">
-                    Create your Amazon Account here!
+                    Create your Amazon Account
                 </button>
             </div>
         </div>
